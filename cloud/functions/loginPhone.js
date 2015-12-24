c1e38f30e1371eecc4f8cb048240b154bbdcefcc
Brian Vallelunga <vallelungabrian@gmail.com>
2015-12-23 20:24:47 -0800

    updated page

--- cloud/functions/loginPhone.js
+++ cloud/functions/loginPhone.js
@@ -6,7 +6,7 @@ Parse.Cloud.define("loginPhone", function(req, res) {
 	query.equalTo("phone", req.params.phone)
 	
 	query.first().then(function(user) {
-		res.success(user.getSessionToken())
+		res.success(user ? user.getSessionToken() : null)
 	}, function(error) {
 		res.error(error.description)
 	})
