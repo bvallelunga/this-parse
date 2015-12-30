9cc2afab548b49b83874e3c1168230d6b57da3f6
Brian Vallelunga <vallelungabrian@gmail.com>
2015-12-30 01:23:39 -0800

    updated words

--- cloud/words/adjectives.js
+++ cloud/words/adjectives.js
@@ -1,19 +1,18 @@
 module.exports = [
 	"red", "green", "blue", "orange", "purple", "gray", "cyan", 
-	"black", "white", "golden", "pink", "yellow", "indigo", "plum",
-	"lime", "teal", "tan", "ivory",
+	"black", "white", "golden", "pink", "yellow", "plum",
+	"lime", "teal", "tan",
 	
 	"curvy", "deep", "flat", "hilly", "thick", "thin", 
-	"big", "fat", "huge", "petite", 
-	"short", "skinny", "small", "tall", "tiny", 
+	"big", "fat", "huge", "short", "small", "tall", "tiny", 
 	"husky", "loud", "mute", "noisy", "quiet", 
 	"soft", "afraid", "angry", "awful", "bad", 
 	"bored", "creepy", "cruel", "evil", "guilty", 
 	"hungry", "lonely", "mad", "scared", "shy", "sleepy", "sore", 
 	"tense", "tired", "upset", "OK", "brave", "bright", 
 	"eager", "funny", "gentle", "good", "happy", "jolly", 
-	"kind", "lucky", "proud",  "silly", "well", "witty", "blonde", 
+	"kind", "lucky", "proud",  "silly", "witty", "blonde", 
 	"bloody", "cute", "dark", "dirty", "dull", 
 	"fancy", "filthy", "homely", "light", "misty", "king",
-	"muddy", "plain", "poised", "quaint", "scary", "ugly"
+	"muddy", "plain", "scary", "ugly"
 ]
