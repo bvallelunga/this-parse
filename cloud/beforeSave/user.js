93d961352371f9930f28cb193f777f8f1c3e543c
Brian Vallelunga <vallelungabrian@gmail.com>
2016-01-24 15:42:14 -0800

    added avatars

--- cloud/beforeSave/user.js
+++ cloud/beforeSave/user.js
@@ -1,4 +1,5 @@
 var Thumbnail = require("cloud/utils/thumbnail")
+var Profile = require("cloud/utils/profile")
 
 Parse.Cloud.beforeSave(Parse.User, function(req, res) {  
   var object = req.object
@@ -6,13 +7,17 @@ Parse.Cloud.beforeSave(Parse.User, function(req, res) {
   Parse.Promise.as().then(function() {
 	  var photo = object.get("photo")
 		
-		if(!object.dirty("photo") || photo == null) {
+		if(photo == null)
+			return Profile()
+		else if(!object.dirty("photo"))
+			return Thumbnail(photo, 250)
+			
+		return false
+	}).then(function(cropped) {
+		if(cropped === false) 
 			return true
-		}
 		
-		return Thumbnail(photo, 250).then(function(cropped) {
-			return object.set("photo", cropped)
-		})
+		return object.set("photo", cropped)
 	}).then(function() {
 	  res.success()
 	}, function(error) {
---
+++ cloud/utils/profile.js
@@ -0,0 +1,22 @@
+var Image = require("parse-image")
+
+module.exports = function() {	
+	return Parse.Config.get().then(function(config) {
+			var random = Math.floor(Math.random() * (52 - 1)) + 1
+		
+		return Parse.Cloud.httpRequest({
+		  url: config.get("host") + "/avatars/" + random + ".jpg"
+		})
+	}).then(function(response) {
+	  var image = new Image()
+	  return image.setData(response.buffer)
+	}).then(function(image) {
+	  return image.setFormat("JPEG")
+	}).then(function(image) {  
+		return image.data()
+	}).then(function(buffer) {
+	  return new Parse.File("thumbnail.jpg", {
+		  base64: buffer.toString("base64") 
+		}).save()
+	})
+}
